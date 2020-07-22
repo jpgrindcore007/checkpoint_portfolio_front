ae2b3c5a4a3a2a8e3c34b70ab9791bcc5bd3caca
jpgrindcore007 <jpsusinirave@gmail.com>
2020-07-22 22:02:59 +0200

    add dependancies, navbar,home

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+// import Router from './components/Router';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jean-paul susini ravé </h1>
+
+        <h2>Développeur Web Junior</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
